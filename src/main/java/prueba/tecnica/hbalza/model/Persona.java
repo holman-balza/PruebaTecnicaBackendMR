5742c96d46361f2a6d5a5b2f12889a5d285dc388
HOLMAN-BALZA <holmanb0402@gmail.com>
2023-01-12 13:42:55 -0500

    first commit

---
+++ src/main/java/prueba/tecnica/hbalza/model/Persona.java
@@ -0,0 +1,89 @@
+package prueba.tecnica.hbalza.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "persona")
+public class Persona {
+	/*
+	 * este id primary key @Id es para agregar las llaves primarias de forma manual
+	 * Pero para generarlo automaticamente agregamos la siguiente linea de
+	 * codigo: @GeneratedValue(strategy = GenerationType.AUTO)
+	 * 
+	 */
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	
+	private int cedula;
+	private String nombres;
+	private String apellidos;
+	private String telefono ;
+	private String direccion;
+	private String email;
+	public Persona(Long id, int cedula, String nombres, String apellidos, String telefono, String direccion,
+			String email) {
+		super();
+		this.id = id;
+		this.cedula = cedula;
+		this.nombres = nombres;
+		this.apellidos = apellidos;
+		this.telefono = telefono;
+		this.direccion = direccion;
+		this.email = email;
+	}
+	public Persona() {
+		super();
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public int getCedula() {
+		return cedula;
+	}
+	public void setCedula(int cedula) {
+		this.cedula = cedula;
+	}
+	public String getNombres() {
+		return nombres;
+	}
+	public void setNombres(String nombres) {
+		this.nombres = nombres;
+	}
+	public String getApellidos() {
+		return apellidos;
+	}
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+	public String getTelefono() {
+		return telefono;
+	}
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+	public String getDireccion() {
+		return direccion;
+	}
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+}
---
+++ src/main/java/prueba/tecnica/hbalza/repository/PersonaRepository.java
@@ -0,0 +1,11 @@
+package prueba.tecnica.hbalza.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import prueba.tecnica.hbalza.model.Persona;
+
+public interface PersonaRepository extends JpaRepository<Persona, Long>{
+
+	
+	
+}
